d34d0a663e19ec51de8960ae4bd161935fdf9e42
start0ys <rladbtjq18@naver.com>
2023-03-19 02:16:16 +0900

    모달 커밋 및 로그인 개발진행

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import './App.css';
 import { Select } from './components/common/select/Select';
+import { Login } from "./components/login/Login";
 
 const App = () => (
-  <>
-    <h1>React without CRA...</h1>
-    <Select label={"테스트"} value={"테스트"} options={[{label:"test11", value:"test"}]} onChange={function (): void {
-      throw new Error("Function not implemented.");
-    } }></Select>
-  </>
+  <Login/>
 );
 
 export default App;
---
+++ src/components/login/Login.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
+import { InputLabel } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: orange[500],
+      },
+    },
+  });
+
+
+export const Login = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                
+                >
+                    <Typography component='h1' variant='h3' color='#fe921f'>
+                        Figure
+                    </Typography>
+                    <TextField 
+                        label='Email' 
+                        name='email'
+                        autoComplete='eamil'
+                        margin='normal'
+                        autoFocus
+                        required
+                        fullWidth
+                    />
+                    <TextField 
+                        label='Password'
+                        name='password' 
+                        type='password' 
+                        margin='normal'
+                        autoComplete='current-password'
+                        required
+                        fullWidth
+                    />
+                    <Grid container>
+                        <Grid item>
+                            <FormControlLabel 
+                                control={<Checkbox value='maintainLogin' color='primary' />} 
+                                label='로그인 유지'
+                            />
+                        </Grid>
+                        <Grid item>
+                            <FormControlLabel 
+                                control={<Checkbox value='saveId' color='primary' />} 
+                                label='아이디 저장'
+                            />
+                        </Grid>
+                    </Grid>
+            
+                    <Button 
+                        type='submit' 
+                        variant='contained' 
+                        size='large'
+                        fullWidth
+                        sx={{ mt: 3, mb:2, color: 'white'}}
+                    >
+                        로그인
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link>회원가입</Link>
+                        </Grid>
+                        <Grid item>
+                            <Link>비밀번호 찾기</Link>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container>
+                        <Grid item xs>
+                            <InputLabel sx={{ mt: 3, mb:3, fontWeight: 'bold'}}>
+                                다른정보로 로그인
+                            </InputLabel>
+                        </Grid>
+                    </Grid>
+                    
+                    <Grid container>
+                        <Grid item xs>
+                            <Button 
+                                type='submit' 
+                                variant='contained' 
+                                size='large'
+                                sx={{color: 'white', backgroundColor: 'rgb(52 152 219)'}}
+                            >
+                                페이스북
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button 
+                                type='submit' 
+                                variant='contained' 
+                                size='large'
+                                sx={{color: 'white', backgroundColor: 'rgb(236, 243, 26)'}}
+                            >
+                                카카오
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    
+                </Box>
+            </Container>
+        </ThemeProvider>
+    )
+}
