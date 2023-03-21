03bb61f3c6936c7613b94b048c6fe318ee8e6f8a
hoondal2 <ghdtjsrud20@gmail.com>
2023-03-22 07:36:31 +0900

    router test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import './App.css';
 import { Select } from './components/common/select/Select';
+import { Routes, Route, Link } from 'react-router-dom'
 
 const App = () => (
   <>
-    <h1>React without CRA...</h1>
-    <Select label={"테스트"} value={"테스트"} options={[{label:"test11", value:"test"}]} onChange={function (): void {
-      throw new Error("Function not implemented.");
-    } }></Select>
+<Link to={"/"}>메인</Link>
+<Link to={"/detail"}>상세화면</Link>
+    
+<Routes>
+      <Route path="/" element={
+        <>
+            <h1>React without CRA...</h1>
+
+            <Select label={"테스트"} value={"테스트"} options={[{label:"test11", value:"test"}]} onChange={function (): void {
+              throw new Error("Function not implemented.");
+            } }></Select>
+        </>
+      }/>
+      <Route path="/detail" element={<div>상세페이지</div>}/>
+      <Route path="/about" element={<div>어바웃페이지</div>}/>
+    </Routes>
   </>
 );
 
