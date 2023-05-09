cb2c352a5ff26da505b525e707cba25f691bf06e
kimyusub <rladbtjq18@jisan.com>
2023-05-09 23:51:07 +0900

    로그인

--- src/actions/userAction.ts
+++ src/actions/userAction.ts
@@ -3,18 +3,27 @@ import { request } from "../utils/axios";
 
 const USER_URL = "/user";
 
+// export function signUpUser(dataToSubmit) {
+//   const data = request("post", USER_URL + "/signUp", dataToSubmit);
+//   return {
+//     type: REGISTER_USER,
+//     payload: data,
+//   };
+// }
+
+// export function loginUser(dataToSubmit) {
+//   const data = request("post", USER_URL + "/login", dataToSubmit);
+//   return {
+//     type: LOGIN_USER,
+//     payload: data,
+//   };
+// }
+
 export function signUpUser(dataToSubmit) {
-  const data = request("post", USER_URL + "/signUp", dataToSubmit);
-  return {
-    type: REGISTER_USER,
-    payload: data,
-  };
+  return request("post", USER_URL + "/signUp", dataToSubmit);
+
 }
 
 export function loginUser(dataToSubmit) {
-  const data = request("post", USER_URL + "/login", dataToSubmit);
-  return {
-    type: LOGIN_USER,
-    payload: data,
-  };
+  return request("post", USER_URL + "/login", dataToSubmit);
 }
