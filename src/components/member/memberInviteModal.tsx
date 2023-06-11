1433b93aa46f4078557d55c89488bf9cd669e608
hoondal2 <ghdtjsrud20@gmail.com>
2023-06-11 17:01:59 +0900

    모달 수정

--- src/components/member/memberInviteModal.tsx
+++ src/components/member/memberInviteModal.tsx
@@ -8,8 +8,9 @@ import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import ShareIcon from '@mui/icons-material/Share';
 import Chip from '@mui/material/Chip';
-import FaceIcon from '@mui/icons-material';
-
+import Input from '@mui/material/Input'
+import FaceIcon, { SpaRounded } from '@mui/icons-material';
+import '../../index.css'
 
 
 const theme = createTheme({
@@ -18,71 +19,126 @@ const theme = createTheme({
         main: orange[500],
       }
     },
+    typography: {
+      // fontFamily: 'NanumSquareEB'
+    }
   });
-    /**
-     * 
-     * 뱃지로 변경하기
-     * 1. 이메일 형식 체크
-     * let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-     * 2. 이메일 형식에 맞다면, 해당 value state에 넣기 -> :: validEmail (드롭다운으로 보일예정)
-     * if(TextField.val())
-     * 3. validEmail에 있는 값 dropdown으로 보여주기
-     * 4. dropDown 클릭하면 badge state로 옮기기
-     * 5. badge state로 옮겨진 값 badge로 보여주기 
-     */
+
+  /**
+   * 
+   * 1. 칩 일정 길이 이상은 아래로 내리기
+   *    일정 글자 수 이상 부터는 줄바꿈한다
+   * 
+   */
+
+
+
 export const MemberInviteModal = (props) => {
-  const [isValidEmail, setIsValidEmail] = useState(false);
-  const [validEmail, setValidMail] = useState('');
-  const [validEmailList, setValidEmailList] = useState([]);
-  const list = ['test1', 'test2'];
 
-  const handleEmailChange = (e) => {
-    const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  const [chips, setChips] = useState([]); 
+  const [email, setEmail] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
+  
+  // 칩 삭제
+  const handleDelete = item => {
+    let newChips = [...chips];
+    newChips = chips.filter(i => i !== item)
+    setChips(newChips);
+  };
 
-    if(emailPattern.test(e.target.value)) {
-      setValidMail(e.target.value);
-      setValidEmailList([e.target.value]);
-    } else {
-      setValidEmailList([]);
+  // 입력 완료
+  const handleKeyDown = evt => {
+    if (["Enter", "Tab", ","].includes(evt.key)) { // 스페이스 바 + 추가/삭제할거 회의 
+      evt.preventDefault();
+
+      const value = email.trim();
+
+      if (value && isValid(value)) {
+        const newChips = [...chips, value];
+        setChips(newChips);
+        
+        setEmail("");
+      }
     }
   };
-  
+
+  // 이메일 검증
+  const isValid = (email) => {
+    let error = null;
+    if (isInList(email)) {
+      error = `이미 추가된 이메일입니다.`;
+    }
+
+    if (!isEmail(email)) {
+      error = `유효한 이메일이 아닙니다.`;
+    }
+
+    if (error) {
+      setErrorMsg(error);
+
+      return false;
+    }
+
+    return true;
+  };
+
+  const isInList = (email) => {
+    return chips.includes(email);
+  };
+
+  const isEmail = (email) => {
+    return /[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/.test(email);
+  };
+
+  // 이메일 입력시 errorMsg지우고
+  const handleChange = evt => {
+    setErrorMsg(null);
+    setEmail(evt.target.value);
+  };
+
+  const handlePaste = evt => {
+    evt.preventDefault(); // 붙여넣기 기본 동작 막기 
+
+    var paste = evt.clipboardData.getData("text");
+    var emails = paste.match(/[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/g);
+
+    if (emails) {
+      var toBeAdded = emails.filter(email => !isInList(email));
+      const newChips = [...chips, ...toBeAdded];
+      setChips(newChips);
+    }
+  };
 
     return (
             <ThemeProvider theme={theme}>
                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', width: '100%', textAlign: 'center' }}>
                 <Typography sx={{fontSize: '15px', fontWeight:'bold', margin:'15px 0', color: '#333'}}>[프로젝트 이름]에 멤버들을 초대해 보세요.</Typography>
-                <Autocomplete 
-                        
-                        id='projectNm' 
-                        sx={{width:'100%', maxLength: 15}}  
-                        options = {validEmailList}
-                        renderInput={(params) => 
-                            <TextField 
-                              placeholder='초대할 멤버의 이메일을 입력해주세요.'  
-                              onChange={handleEmailChange}
-                              {...params} 
-                              InputProps={{
-                                startAdornment: (
-                                  <>
-                                    {params.InputProps.endAdornment}
-                                    <Chip label="Deletable" onDelete={() => {}} />
-                                  </>
-                                )
-                              }}
-                          /> }
-                        
+               
+                <Box sx={{ display: 'flex'}}>
+                  {chips.map(item => (
+                      <div className="tag-item" key={item}>
+                        <Chip label={item} onDelete={()=> handleDelete(item)}/>
+                      </div>
+                  ))}
+                </Box>
 
+                <Input
+                  value={email}
+                  placeholder="이메일을 입력하고 엔터키를 눌러주세요."
+                  onKeyDown={handleKeyDown}
+                  onChange={handleChange}
+                  onPaste={handlePaste}
+                  sx={{width:'100%'}}
+                />
+                {errorMsg && <p className="error">{errorMsg}</p>}
 
-
-                 />
 					        <div style={{width: '100%', display:'flex', justifyContent:'flex-end', alignItems:'center', fontSize:'15px', marginTop:'5px',cursor:'pointer'}}>
                     <ShareIcon sx={{fontSize:'18px'}}/>&nbsp;
-                        <Link underline='always'>초대 링크 공유</Link>&nbsp;
-                    </div>
-                    <div style={{width: '100%', marginTop: '20px'}}>
-                        <Button label='초대하기' style={{width:'50%', padding: '12px 0', margin: '15px 0', fontSize: '16px', fontWeight:'bold'}}/>
-                    </div>
+                    <Link underline='always'>초대 링크 공유</Link>&nbsp;
+                  </div>
+                  <div style={{width: '100%', marginTop: '20px'}}>
+                    <Button label='초대하기' style={{width:'50%', padding: '12px 0', margin: '15px 0', fontSize: '16px', fontWeight:'bold'}}/>
+                  </div>
               </Box>
             </ThemeProvider>
     )
--- src/components/project/CreateProject.tsx
+++ src/components/project/CreateProject.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from '../common/button/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { orange } from '@mui/material/colors';
@@ -15,25 +15,113 @@ const theme = createTheme({
   });
 
 export const CreateProject = (props) => {
+
+	const [projectNm, setProjectNm] = useState('');
+	const [projectDesc, setProjectDesc] = useState('');
+
+	const handleCreateProject = () => {
+
+	}
+
     return (
             <ThemeProvider theme={theme}>
-               <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%', textAlign: 'center' }}>
+               <Box 
+			   		sx={{ 
+						display: 'flex', 
+						flexDirection: 'column', 
+						justifyContent: 'center', 
+						alignItems: 'center', 
+						width: '100%', 
+						textAlign: 'center' 
+						}}
+				>
 					<div>
-						<Typography variant='h4' sx={{fontWeight:'bold', color: '#333'}}>프로젝트 생성하기</Typography>
-						<Typography sx={{fontSize: '15px', fontWeight:'bold', margin:'15px 0', color: '#333'}}>프로젝트 정보를 입력하고, 프로젝트 관리자로 시작해 보세요.</Typography>
+						<Typography 
+							variant='h4' 
+							sx={{
+								fontWeight:'bold', 
+								color: '#333'
+								}}
+						>
+							프로젝트 생성하기
+						</Typography>
+						<Typography 
+							sx={{
+								fontSize: '15px', 
+								fontWeight:'bold', 
+								margin:'15px 0', 
+								color: '#333'}}
+						>
+							프로젝트 정보를 입력하고, 프로젝트 관리자로 시작해 보세요.
+						</Typography>
 					</div>
-					<Box sx={{marginTop: '20px', width: '70%'}}>
-						<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '10px'}}>
-							<InputLabel htmlFor="projectNm" sx={{fontWeight:'bold', width:'30%', color: '#333'}} required>프로젝트 이름</InputLabel>
-							<TextField sx={{width:'70%', maxLength: 15}} required placeholder='프로젝트 이름을 입력해주세요.' id='projectNm' />
+					<Box 
+						sx={{marginTop: '20px', width: '70%'}}>
+						<Box 
+							sx={{ 
+								display: 'flex', 
+								justifyContent: 'center', 
+								alignItems: 'center', 
+								marginBottom: '10px'
+								}}
+						>
+							<InputLabel 
+								htmlFor="projectNm" 
+								sx={{fontWeight:'bold', width:'30%', color: '#333'}} 
+								required
+							>
+								프로젝트 이름
+							</InputLabel>
+							<TextField 
+								sx={{width:'70%', maxLength: 15}} 
+								required 
+								placeholder='프로젝트 이름을 입력해주세요.' 
+								id='projectNm'
+								value={projectNm} 
+							/>
 						</Box>
 					
-						<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '10px'}}>
-							<InputLabel htmlFor="projectDesc" sx={{fontWeight:'bold', width:'30%', marginBottom: '100px', color: '#333'}} required>프로젝트 설명</InputLabel>
-							<TextField sx={{width:'70%',  '& input' : {maxLength:50} }} multiline rows={5} required placeholder='프로젝트 설명을 입력해주세요.' id='projectDesc' />
+						<Box 
+							sx={{ 
+								display: 'flex', 
+								justifyContent: 'center', 
+								alignItems: 'center', 
+								marginBottom: '10px'
+								}}
+						>
+							<InputLabel 
+								htmlFor="projectDesc" 
+								sx={{
+									fontWeight:'bold', 
+									width:'30%', 
+									marginBottom: '100px', 
+									color: '#333'
+								}} 
+								required
+							>
+								프로젝트 설명
+							</InputLabel>
+							<TextField 
+								sx={{width:'70%',  '& input' : {maxLength:50} }}
+								multiline 
+								rows={5} 
+								required 
+								placeholder='프로젝트 설명을 입력해주세요.' 
+								id='projectDesc'
+								value={projectDesc} 
+							/>
 						</Box>
-					
-						<Button label='프로젝트 생성' style={{width: '70%', padding: '12px 0', margin: '15px 0', fontSize: '16px', fontWeight:'bold'}}/>
+						<Button 
+							label='프로젝트 생성' 
+							style={{
+								width: '70%', 
+								padding: '12px 0', 
+								margin: '15px 0', 
+								fontSize: '16px', 
+								fontWeight:'bold'
+							}}
+							onClick = {handleCreateProject}
+						/>
 					</Box>
               </Box>
             </ThemeProvider>
